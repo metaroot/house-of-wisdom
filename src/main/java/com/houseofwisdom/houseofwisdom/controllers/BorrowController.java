3afa6063a2d971b52c2f1d995e99862821398581
metaroot <faisal.ahmed.cse.sust@gmail.com>
2021-10-12 16:07:29 +0600

    Write logs in the borrow controller and implement error handling

--- src/main/java/com/houseofwisdom/houseofwisdom/controllers/BorrowController.java
+++ src/main/java/com/houseofwisdom/houseofwisdom/controllers/BorrowController.java
@@ -1,41 +1,74 @@
 package com.houseofwisdom.houseofwisdom.controllers;
 
 import com.houseofwisdom.houseofwisdom.dto.BookDTO;
-import com.houseofwisdom.houseofwisdom.model.Book;
-import com.houseofwisdom.houseofwisdom.model.Borrow;
 import com.houseofwisdom.houseofwisdom.model.User;
 import com.houseofwisdom.houseofwisdom.service.BorrowService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/borrow")
 public class BorrowController {
+    private static final Logger logger = LoggerFactory.getLogger(BorrowController.class);
     @Autowired
     BorrowService borrowService;
 
     @PostMapping("/take")
     public Long borrowBook(@RequestParam Long userId,
                            @RequestParam Long bookId) {
-        return borrowService.borrowBook(userId, bookId);
+        Long borrowId = null;
+        try {
+            borrowId = borrowService.borrowBook(userId, bookId);
+            logger.info("User id: " + userId.toString() + "borrowed book id: " + bookId.toString());
+        } catch (Exception ex) {
+            logger.info("User id: " + userId.toString() + "couldn't borrow book id: " + bookId.toString());
+            ex.printStackTrace();
+        }
+        return borrowId;
     }
 
     @PostMapping("/return")
     public void returnBook(@RequestParam Long userId,
                            @RequestParam Long bookId) {
-        borrowService.returnBook(userId, bookId);
+        try {
+            borrowService.returnBook(userId, bookId);
+            logger.info("User id: " + userId.toString() + "returned book id: " + bookId.toString());
+        } catch (Exception ex) {
+            logger.info("User id: " + userId.toString() + "couldn't return book id: " + bookId.toString());
+            ex.printStackTrace();
+        }
+
     }
 
     @GetMapping("/list-of-users-by-book/{bookId}")
     public List<User> listOfUsersByBook(@PathVariable Long bookId) {
-        return  borrowService.listOfUsersByBook(bookId);
+        List<User> users = new ArrayList<>();
+        try {
+            users = borrowService.listOfUsersByBook(bookId);
+            logger.info("Successfully fetched list of users with book id: " + bookId.toString());
+        } catch (Exception ex) {
+            logger.info("Couldn't fetch list of users with book id: " + bookId.toString());
+            ex.printStackTrace();
+        }
+        return  users;
     }
 
     @GetMapping("/list-of-books-by-user/{userId}")
     public List<BookDTO> listOfBooksByUser(@PathVariable Long userId) {
-        return  borrowService.listOfBooksByUser(userId);
+        List<BookDTO> bookDTOS = new ArrayList<>();
+        try {
+            bookDTOS = borrowService.listOfBooksByUser(userId);
+            logger.info("Successfully fetched list of books with user id: " + userId.toString());
+        } catch (Exception ex) {
+            logger.info("Couldn't fetch list of books with user id: " + userId.toString());
+            ex.printStackTrace();
+        }
+        return  bookDTOS;
     }
 }
